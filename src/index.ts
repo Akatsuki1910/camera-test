fd5b539caa354b59cae2d197158d9c58a3a21c12
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2023-05-16 18:35:51 +0900

    変更

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,53 @@
-const cameraInitSmartphoneSupport = () => {
+const cameraInitSmartphoneSupport = async () => {
   const video = document.getElementById('camera') as HTMLVideoElement;
 
-  const cameraSetting = {
-    audio: false,
-    video: { facingMode: { exact: 'environment' } },
-  };
+  const enumerate = await navigator.mediaDevices.enumerateDevices();
+  const d = document.getElementById('data') as HTMLDivElement;
+  const s = document.getElementById('s') as HTMLSelectElement;
+  s.innerHTML = '';
+  let f = '';
+  if (d) {
+    for (const k of enumerate) {
+      if (k.kind === 'videoinput') {
+        f += '##############\n';
+        f += `deviceId: ${k.deviceId}\n`;
+        f += `groupId: ${k.groupId}\n`;
+        f += `kind: ${k.kind}\n`;
+        f += `label: ${k.label}\n`;
+        f += '##############\n';
 
-  navigator.mediaDevices.enumerateDevices().then((e) => {
-    const d = document.getElementById('data') as HTMLDivElement;
-    let f = '';
-    if (d) {
-      for (const k of e) {
-        if (k.kind === 'videoinput') {
-          f += '##############\n';
-          f += `deviceId: ${k.deviceId}\n`;
-          f += `groupId: ${k.groupId}\n`;
-          f += `kind: ${k.kind}\n`;
-          f += `label: ${k.label}\n`;
-          f += '##############\n';
-        }
+        s.innerHTML += `<option value=${k.deviceId}>${k.label}</option>`;
       }
     }
-    d.innerText = f;
-  });
+  }
+  d.innerText = f;
 
-  navigator.mediaDevices
-    .getUserMedia(cameraSetting)
-    .then((mediaStream) => {
-      if (video) {
-        const d = document.getElementById('now') as HTMLDivElement;
-        let f = '';
-        if (d) {
-          f += '@@@@@@@@@@@@@@@\n';
-          f += mediaStream.id + '\n';
-          f += '@@@@@@@@@@@@@@@\n';
-          d.innerHTML = f;
-        }
-        video.srcObject = mediaStream;
-      }
-    })
-    .catch((err) => {
+  s.onchange = async (e) => {
+    let getD;
+    try {
+      getD = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: { deviceId: this.value },
+      });
+    } catch (err) {
       const d = document.getElementById('err') as HTMLDivElement;
       if (d) {
         d.innerHTML = err.toString();
       }
-    });
+    }
+
+    if (video) {
+      // const d = document.getElementById('now') as HTMLDivElement;
+      // let f = '';
+      // if (d) {
+      //   f += '@@@@@@@@@@@@@@@\n';
+      //   f += getD + '\n';
+      //   f += '@@@@@@@@@@@@@@@\n';
+      //   d.innerHTML = f;
+      // }
+      video.srcObject = getD;
+    }
+  };
+  s.options[0].selected = true;
+  s.dispatchEvent(new Event('change'));
 };
